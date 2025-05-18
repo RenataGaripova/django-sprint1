cb4b98c828a37a800977b9506e5ad3a9bf15242f
Renata Garipova <rena.garipova.2005@gmail.com>
2025-05-18 16:05:46 +0500

    pep8

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -59,12 +59,11 @@ def post_detail(request, id):
         'post': posts[id],
     }
     return render(request, template_name, context)
-    
 
 
 def category_posts(request, category_slug):
     template_name = 'blog/category.html'
-    context =  {
+    context = {
         'slug': category_slug,
     }
     return render(request, template_name, context)
